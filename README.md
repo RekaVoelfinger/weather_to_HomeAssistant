6f0c3cb88e8b01b990787d5cd4f9abd0f6cd138b
vreka <68538271+RekaVoelfinger@users.noreply.github.com>
2020-10-02 11:05:14 +0200

    Chance of rain, date and time are retrieved from response

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # weather_to_HomeAssistant
 Collect weather data from different webseites and make a card from it in HomeAssistant
+
+SOURCES
+
+OpenWeather
+-----------
+Parameters see on: https://openweathermap.org/api/one-call-api
+Blaustein-Wiedach geographic coordinates lat=48.445653, lon=9.887470
+
+To get access to current weather, minute forecast for 1 hour, hourly forecast for 48 hours, daily forecast for 7 days and government weather alerts.
+For hourly and daily forecast only add parameter 'exclude=current,minutely'
+response_all_info = requests. get('http://api.openweathermap.org/data/2.5/onecall?lat=48.45&lon=9.89&units=metric&appid=6ec4e9ae0169e18bdf68974e631274cc')
+Eg.
+Response current weather in Weidach
+# response = requests. get('http://api.openweathermap.org/data/2.5/weather?lat=48.45&lon=9.89&units=metric&appid=6ec4e9ae0169e18bdf68974e631274cc')
--- open_weather.py
+++ open_weather.py
@@ -1,19 +1,8 @@
 import requests
 import json
 import configparser
+from datetime import datetime
 
-'''
-OpenWeather
-Parameters see on: https://openweathermap.org/api/one-call-api
-Blaustein-Wiedach geographic coordinates lat=48.445653, lon=9.887470
-
-To get access to current weather, minute forecast for 1 hour, hourly forecast for 48 hours, daily forecast for 7 days and government weather alerts.
-For hourly and daily forecast only add parameter 'exclude=current,minutely'
-response_all_info = requests. get('http://api.openweathermap.org/data/2.5/onecall?lat=48.45&lon=9.89&units=metric&appid=6ec4e9ae0169e18bdf68974e631274cc')
-Eg.
-Response current weather in Weidach
-# response = requests. get('http://api.openweathermap.org/data/2.5/weather?lat=48.45&lon=9.89&units=metric&appid=6ec4e9ae0169e18bdf68974e631274cc')
-'''
 
 def get_api_key():
     config = configparser.ConfigParser()
@@ -23,8 +12,8 @@ def get_api_key():
 
 def get_weather(api_key, lattitude, longitude):
     url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lattitude}&lon={longitude}&units=metric&appid={api_key}"
-    r = requests.get(url)
-    return r.json()
+    r = requests.get(url) # 'requests.models.Response' type
+    return r.json() # returns 'dict' type
 
 
 # for anlysis write the response to file
@@ -32,13 +21,31 @@ def write_to_file(file_name, response):
     with open(file_name, "w") as write_file:
         write_file.write(str(response))
 
+# TODO elaborate the result of will_it_rain function
+def will_it_rain(data_list, time_of_query): # call it in main with data_all_hours
+    rainy_hours = {}
+    count = 0
+    for n in data_list:
+        if data_list[n]['pop'] != '0':
+            rainy_hours[str(int(time_of_query) + count)] = data_list[n]['pop']
+    return rainy_hours
+
+
+
 # Weidach coordinates (lat,lon): (48.45, 9.89)
 def main():
     api_key = get_api_key()
     forecast = get_weather(api_key, "48.45", "9.89")
-   # write_to_file("response.json", forecast)
-    print(forecast['hourly']['weather'])
-
+    data_all_hours = forecast['hourly'] # list type
+    # print(f"data from hourly: {data_all_hours}")
+    # print(data_all_hours[:11]) # forecast for the next 12 hour
+    date_of_query = datetime.utcfromtimestamp(data_all_hours[0]['dt'] + forecast['timezone_offset']).strftime('%Y-%m-%d')
+    time_of_query = datetime.utcfromtimestamp(data_all_hours[0]['dt'] + forecast['timezone_offset']).strftime('%H:%M')
+    print(f"date_of_query: {date_of_query}") # TODO these prints belong to debug and log file
+    print(f"time_of_query: {time_of_query}")
+    print(f"Chance of rain: {100 * data_all_hours[1]['pop']}%")
+    print(f"Temperature: {data_all_hours[1]['temp']}")
+    print(f"Time: {datetime.utcfromtimestamp(data_all_hours[1]['dt']).strftime('%H:%M')}")
 
 if __name__ == '__main__':
     main()
