8c408c6120aa241b00689cbce99c939b79fdb439
vreka <68538271+RekaVoelfinger@users.noreply.github.com>
2020-10-09 11:12:24 +0200

    AccuWeather responses the rainy hours.
    Still with separate will_it_rain function.

--- api_helper.py
+++ api_helper.py
@@ -1,8 +1,18 @@
 import configparser
+import requests
 
 def get_api_key(service_name):
     config = configparser.ConfigParser()
     config.read('config.ini')
     return config[service_name]['api_key']
+
+# AccuWeather needs to get Location Key for given (latitude,longitude)
+# Weidach coordinates are (lat,lon): (48.45, 9.89)
+def get_location_key(api_key, lat, lon):
+    url = f"http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey={api_key}&details=true&q=" + lat +"," + lon
+    response_location_key = requests.get(url)
+    location = response_location_key.json()
+    print(f"Location Key for {location['LocalizedName']}: {location['Key']}")
+    return location['Key']
 
 
--- open_weather.py
+++ open_weather.py
@@ -3,11 +3,17 @@ import requests
 import json
 from datetime import datetime
 
-
-def get_weather(api_key, lattitude, longitude):
+# Get weather data from OpenWeatherMap
+def get_weather_ow(api_key, lattitude, longitude):
     url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lattitude}&lon={longitude}&units=metric&appid={api_key}"
     r = requests.get(url) # 'requests.models.Response' type
     return r.json() # returns 'dict' type
+
+# Get weather data from AccuWeather
+def get_weather_acw(api_key, location_key):
+    url = f"http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/{location_key}?apikey={api_key}&details=true&metric=true"
+    r = requests.get(url)  # 'requests.models.Response' type
+    return r.json()  # returns 'dict' type
 
 
 # for anlysis write the response to file
@@ -16,34 +22,55 @@ def write_to_file(file_name, response):
         write_file.write(str(response))
 
 # Return a dictionery with the predicted time and probability of rain
-def will_it_rain(data_list, start_time, hours): # call it in main with data_all_hours, time_to_int(time_of_query)
+def will_it_rain(data_list, start_time, hours): # call it in main with data_all_hours_ow, time_to_int(time_of_query_ow)
     rainy_hours = {}
-    print(f"start_time: {start_time}") # TODO why start_time = 1 ???
+    print(f"start_time: {start_time}")
     for h in range(hours):
         print(f"h: {h} : pop: {data_list[h]['pop']}")
         if data_list[h]['pop'] != 0:
             rainy_hours[str(start_time + h)] = data_list[h]['pop']
     return rainy_hours
 
-# Convert time string from time_of_query to integer
+# TODO create a general function from will_it_rain
+def will_it_rain_acw(data_list, start_time, hours): # call it in main with forecast_acw, time_to_int(time_of_query_ow)
+    rainy_hours = {}
+    print(f"start_time: {start_time}")
+    for h in range(hours):
+        print(f"h: {h} : pop: {data_list[h]['RainProbability']}")
+        if data_list[h]['RainProbability'] != 0:
+            rainy_hours[str(start_time + h)] = data_list[h]['RainProbability']
+    return rainy_hours
+
+# Convert time string from time_of_query to integer.
 def time_to_int(time_str):
     return int(time_str[:2])
 
 # Weidach coordinates (lat,lon): (48.45, 9.89)
 def main():
-    api_key_ow = api_helper.get_api_key('openweathermap')
-    forecast = get_weather(api_key_ow, "48.45", "9.89")
-    data_all_hours = forecast['hourly'] # list type
-    # print(f"data from hourly: {data_all_hours}")
-    # print(data_all_hours[:11]) # forecast for the next 12 hour
-    date_of_query = datetime.utcfromtimestamp(data_all_hours[0]['dt'] + forecast['timezone_offset']).strftime('%Y-%m-%d')
-    time_of_query = datetime.utcfromtimestamp(data_all_hours[0]['dt'] + forecast['timezone_offset']).strftime('%H:%M')
-    print(f"date_of_query: {date_of_query}") # TODO these prints belong to debug and log file
-    print(f"time_of_query: {time_of_query}")
-    print(f"Chance of rain: {100 * data_all_hours[1]['pop']}%")
-    print(f"Temperature: {data_all_hours[1]['temp']}")
-    print(f"Rainy hours: {will_it_rain(data_all_hours, time_to_int(time_of_query), 12)}")
 
+    # OpenWeatherMap
+    api_key_ow = api_helper.get_api_key("openweathermap")
+    forecast_ow = get_weather_ow(api_key_ow, "48.45", "9.89")
+    data_all_hours_ow = forecast_ow['hourly'] # list type
+    # print(f"data from hourly: {data_all_hours_ow}")
+    # print(data_all_hours_ow[:11]) # forecast for the next 12 hour
+    date_of_query_ow = datetime.utcfromtimestamp(data_all_hours_ow[0]['dt'] + forecast_ow['timezone_offset']).strftime('%Y-%m-%d')
+    time_of_query_ow = datetime.utcfromtimestamp(data_all_hours_ow[0]['dt'] + forecast_ow['timezone_offset']).strftime('%H:%M')
+    print(f"date_of_query_ow: {date_of_query_ow}") # TODO these prints belong to debug and log file
+    print(f"time_of_query_ow: {time_of_query_ow}")
+    print(f"Chance of rain: {100 * data_all_hours_ow[1]['pop']}%")
+    print(f"Temperature: {data_all_hours_ow[1]['temp']}")
+    print(f"Rainy hours: {will_it_rain(data_all_hours_ow, time_to_int(time_of_query_ow), 12)}")
+
+
+    # AccuWeather
+    api_key_acw = api_helper.get_api_key("accuweather")
+    # location_key = "986294"  # Weidach
+    location_key = api_helper.get_location_key(api_key_acw, "48.45", "9.89")
+    forecast_acw = get_weather_acw(api_key_acw, location_key)
+    time_of_query_acw = datetime.utcfromtimestamp(forecast_acw[0]['EpochDateTime'] + 7200).strftime('%H:%M') # TODO time can be common with OpenWeather
+    print(f"time_of_query: {time_of_query_acw}")
+    print(f"Rainy hours: {will_it_rain_acw(forecast_acw, time_to_int(time_of_query_acw), 12)}") #TODO time to clarify by the different services
 
 if __name__ == '__main__':
     main()
